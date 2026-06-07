d72e43997785b708762217e3ded9ab9965e7c34d
Harshit Sinha <harshitsinha@Harshits-MacBook-Air.local>
2026-06-07 09:43:06 +0530

    Adad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero'; // Agar hero pehle banaya tha
 import FeaturedProducts from '@/components/FeaturedProducts';
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      <Navbar />
       <Hero /> 
       <FeaturedProducts />
     </main>
